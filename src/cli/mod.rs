f025524459ea38fc28ec0300af6200f1358650fe
Kevin Harnata <kevin.harnata@gdn-commerce.com>
2021-06-12 17:27:19 +0700

    changing something hehe

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Kevin Jonathan Harnanta <kevin.harnata@gdn-commerce.com>
+Copyright © 2021 Kevin Jonathan Harnanta <crossmajor99@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
